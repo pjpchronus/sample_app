e997f30194109dbe6b46870dd2a3f1eba3c07b54
pjpchronus <jyothi.prakash@chronus.com>
2018-07-02 16:39:54 +0530

    User models finished

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source 'https://rubygems.org'
 
 gem 'rails',        '5.1.4'
+gem 'bcrypt',         '3.1.12'
 gem 'bootstrap-sass', '3.3.7'
 gem 'puma',         '3.9.1'
 gem 'sass-rails',   '5.0.6'
@@ -11,7 +12,7 @@ gem 'turbolinks',   '5.0.1'
 gem 'jbuilder',     '2.7.0'
 
 group :development, :test do
-  gem 'sqlite3', '1.3.13'
+  gem 'mysql2', '>= 0.3.18', '< 0.5'
   gem 'byebug',  '9.0.6', platform: :mri
 end
 
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,5 @@
 ENV['RAILS_ENV'] ||= 'test'
+ActiveRecord::Migration.maintain_test_schema!
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
@@ -6,6 +7,5 @@ class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
   include ApplicationHelper
-
   # Add more helper methods to be used by all tests here...
 end
