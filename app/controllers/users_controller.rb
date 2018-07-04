57db493b7af3d955a7360e01c7390c07fd269081
pjpchronus <jyothi.prakash@chronus.com>
2018-07-04 18:59:36 +0530

    following users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -42,7 +42,20 @@ class UsersController < ApplicationController
 	      render 'edit'
 	    end
 	 end
-	
+	def following
+    @title = "Following"
+    @user  = User.find(params[:id])
+    @users = @user.following.paginate(page: params[:page])
+    render 'show_follow'
+  end
+
+  def followers
+    @title = "Followers"
+    @user  = User.find(params[:id])
+    @users = @user.followers.paginate(page: params[:page])
+    render 'show_follow'
+  end
+
 	private
 
 	def admin_user
